00bb2560531b4e5e61ab69a568f8e5ba09f8ba5f
bencev93 <velmovszkibence@gmail.com>
2021-04-26 11:06:40 +0200

    update

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -5,16 +5,13 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\PaymentRequest;
+use Stripe\Stripe;
+use Stripe\Charge;
 
 class PaymentController extends Controller
 {
     public function postPayment(PaymentRequest $request)
     {
-        if(!Session::has('cart')) {
-            return view('shop.shopping-cart');
-        }
-        $prevCart = Session::get('cart');
-        $cart = new Cart($prevCart);
 
         // Charge with stripe and save order to db
         Stripe::setApiKey('sk_test_51HHWrwKg112ZJOxi1vzTpYho8tHcffJotwlTGz0RYxu55fpD6bxPCdwq7uTJs0cOlqp913sl7Nq47UpM66M5SWAM00nTsiIIF3');
@@ -25,16 +22,16 @@ class PaymentController extends Controller
             'phone' => $request->phone,
             'address' => $request->address,
             'city' => $request->city,
-            'zipcode' => $request->zip
+            'zipcode' => $request->zip,
             'country' => $request->country
         ];
 
         try {
             $charge = Charge::create([
-                'amount' => $request->totalPrice * 100,
+                'amount' => $request->total * 100,
                 'currency' => 'usd',
                 'description' => 'Example charge',
-                'source' => $request['stripeToken'],
+                'source' => $request->stripeToken,
             ]);
 
             $order = Order::create([
@@ -46,30 +43,32 @@ class PaymentController extends Controller
                 'country' => $input['country'],
                 'email' => $input['email'],
                 'phone' => $input['phone'],
-                'cart' => serialize($cart),
+                'cart' => serialize($request->items),
                 'payment_id' => $charge->id
             ]);
 
             !Auth::user() ? $order->save() : Auth::user()->orders()->save($order);
 
         } catch(Exception $e) {
-            return redirect()->route('checkout')->with('error', $e->getMessage());
+            $error = $e->getMessage();
+            return response()->json(['error' => $error, 'request' => $request], 500);
         }
 
-        Mail::to($input['email'])->send(new OrderUnderProcess($input['name'], $cart));
+        // Mail::to($input['email'])->send(new OrderUnderProcess($input['name'], $cart));
 
         // Count how many times product sold
-        while(count($cart->items) != 0) {
-            $soldqty = array_values($cart->items)[0]['quantity'];
-            $soldid = array_values($cart->items)[0]['item']->id;
-            $product = Product::find($soldid);
-            $product->sold = $product->sold + $soldqty;
-            $product->save();
+        // while(count($request->items) != 0) {
+        //     $soldqty = array_values($cart->items)[0]['quantity'];
+        //     $soldid = array_values($cart->items)[0]['item']->id;
+        //     $product = Product::find($soldid);
+        //     $product->sold = $product->sold + $soldqty;
+        //     $product->save();
+        //
+        //     \array_splice($cart->items, 0, 1);
+        // }
 
-            \array_splice($cart->items, 0, 1);
-        }
 
-        Session::forget('cart');
-        return redirect()->route('product.index')->with('success', 'Your order created successfully. Thank you for choosing us!');
+        // return redirect()->route('product.index')->with('success', 'Your order created successfully. Thank you for choosing us!');
+        return response()->json(null, 200);
     }
 }
--- app/Http/Requests/PaymentRequest.php
+++ app/Http/Requests/PaymentRequest.php
@@ -9,13 +9,10 @@ class PaymentRequest extends FormRequest
     public function rules()
     {
 
-        // Charge with stripe and save order to db
-        Stripe::setApiKey('sk_test_51HHWrwKg112ZJOxi1vzTpYho8tHcffJotwlTGz0RYxu55fpD6bxPCdwq7uTJs0cOlqp913sl7Nq47UpM66M5SWAM00nTsiIIF3');
-
         return [
             'name' => 'required|min:5|max:30|string',
             'email' => 'required|email',
-            'phone' => 'required|numeric|digits_between:9,30'
+            'phone' => 'required|numeric|digits_between:9,30',
             'address' => 'required|min:5|max:30|string',
             'city' => 'required|min:3|max:30|string',
             'zip' => 'required|numeric|digits_between:1,5',
